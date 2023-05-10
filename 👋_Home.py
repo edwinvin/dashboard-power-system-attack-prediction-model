7738201b318cd426d954b9316caae7cd9b10f7b1
edwinvin <120187979+edwinvin@users.noreply.github.com>
2023-05-11 03:27:35 +0800

    Add files via upload

---
+++ 👋_Home.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Home",
+    page_icon="👋",
+    layout="wide"
+)
+
+st.title("Network Intrusion Detection Model based on Power System Attack Datasets using Logistic Regression Algorithm")
+
+st.markdown("***")
+st.markdown(""" 
+
+The Network Intrusion Detection Model is developed to provide classification of power system attack (Data Injection Attack) occurrence 
+through Machine Learning implementation of Logistic Regression Algorithm.
+
+#### Dataset Information
+- The Daatset is an open source simulated power system dataset provided by Mississippi State University Oak Ridge National 
+Laboratories which contains three datasets that comprise measurements of the regular, disturbed, controlled, and cyberattack 
+behaviours of electric transmission systems whereby the collection contains measurements from relays, a simulated control panel, 
+synchrophasor measurements, and data logs from Snort.
+
+- Source: https://sites.google.com/a/uah.edu/tommy-morris-uah/ics-data-sets
+
+- The Dataset is filtered to only include Natural Events and Data Injection Attack Events.
+- More Information on Dataset: http://www.ece.uah.edu/~thm0009/icsdatasets/PowerSystem_Dataset_README.pdf """)
+
+st.write("")
+st.markdown(""":point_left: **Select a module from the sidebar** to see and utilize functions available in the dashboard for the 
+intrusion detection model.
+
+#### Dashboard Module 
+- Data Visualization
+- Machine Learning Model Information
+- Power System Attack Prediction  """)
+
+st.sidebar.success("Select a module above.")
