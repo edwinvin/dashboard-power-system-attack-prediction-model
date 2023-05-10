ba4c0dc1152174926a8a9a8ccbb96845f994a5b9
edwinvin <120187979+edwinvin@users.noreply.github.com>
2023-05-11 04:23:53 +0800

    Update 2_📝_Model_Information.py

--- pages/2_📝_Model_Information.py
+++ pages/2_📝_Model_Information.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
-import matplotlib.pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay
 import pickle
 
